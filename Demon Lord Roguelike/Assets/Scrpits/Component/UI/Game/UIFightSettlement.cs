c9146729b4ab4ea169a77f97b50f5808d48a6cc2
Apple\AppleCoffee <734719526@qq.com>
2025-02-15 00:28:02 +0800

    UI修改

--- Demon Lord Roguelike/Assets/Scrpits/Component/UI/Game/UIFightSettlement.cs
+++ Demon Lord Roguelike/Assets/Scrpits/Component/UI/Game/UIFightSettlement.cs
@@ -1,7 +1,23 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public partial class UIFightSettlement : BaseUIComponent
 {
+    public override void OnClickForButton(Button viewButton)
+    {
+        base.OnClickForButton(viewButton);
+        if (viewButton == ui_ViewExit)
+        {
+            OnClickForExit();
+        }
+    }
 
+    /// <summary>
+    /// 点击退出
+    /// </summary>
+    public void OnClickForExit()
+    {
+
+    }
 }
--- Demon Lord Roguelike/Assets/Scrpits/Component/UI/Game/UIFightSettlementComponent.cs
+++ Demon Lord Roguelike/Assets/Scrpits/Component/UI/Game/UIFightSettlementComponent.cs
@@ -5,5 +5,15 @@ public partial class UIFightSettlement
 {
     public Button ui_ViewExit;
 
+    public Button ui_OrderBtn_Strike;
+
+    public Button ui_OrderBtn_Exp;
+
+    public ScrollGridVertical ui_List;
+
+    public UIViewBaseInfoContent ui_ViewBaseInfoContent;
+
+    public Button ui_OrderBtn_Sustain ;
+
 
 }
